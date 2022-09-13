7f1d4f3006fa0a8c9c9c093501874ddc0228dd35
gonzalo-cordova-pou <gonzalocp6@gmail.com>
2022-09-13 16:06:52 +0200

    Starting to import packages

--- utils.py
+++ utils.py
@@ -21,34 +21,3 @@ stopwords_english = stopwords.words('english')
 from nltk.stem import PorterStemmer
 stemmer = PorterStemmer()
 
-
-def process_tweet(tweet):
-    '''
-    Input: 
-        tweet: a string containing a tweet
-    Output:
-        tweets_clean: a list of words containing the processed tweet
-    
-    '''
-    # remove stock market tickers like $GE
-    tweet = re.sub(r'\$\w*', '', tweet)
-    # remove old style retweet text "RT"
-    tweet = re.sub(r'^RT[\s]+', '', tweet)
-    # remove hyperlinks
-    tweet = re.sub(r'https?:\/\/.*[\r\n]*', '', tweet)
-    # remove hashtags
-    # only removing the hash # sign from the word
-    tweet = re.sub(r'#', '', tweet)
-    # tokenize tweets
-    tokenizer = TweetTokenizer(preserve_case=False, strip_handles=True, reduce_len=True)
-    tweet_tokens = tokenizer.tokenize(tweet)
-    ### START CODE HERE ###
-    tweets_clean = []
-    for word in tweet_tokens:
-        if (word not in stopwords_english and # remove stopwords
-            word not in string.punctuation): # remove punctuation
-            #tweets_clean.append(word)
-            stem_word = stemmer.stem(word) # stemming word
-            tweets_clean.append(stem_word)
-    ### END CODE HERE ###
-    return tweets_clean

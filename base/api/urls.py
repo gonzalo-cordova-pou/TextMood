901d77409733e8579dbf38c87b96ba689acc0f5d
RicardTarre <rtarremira@gmail.com>
2022-10-18 11:32:49 +0100

    Modified urls

--- base/api/urls.py
+++ base/api/urls.py
@@ -2,5 +2,6 @@ from  django.urls import path
 from . import views
 
 urlpatterns = [
-    path('base/', views.getClassification, ),
+    path('', views.getRoutes, ),
+    path('classify/', views.getClassification, ),
 ]
